76a93f103374b08a4c73288e4b42f8f1f5679ae2
ahdianmirza <ahdianmirza@gmail.com>
2023-11-28 11:35:51 +0700

    Memperbaiki tampilan login dan register

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -4,13 +4,22 @@
     <main>
         <div class="container-fluid">
             <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center">
-                <div class="container-fluid">
-                    <div class="row justify-content-center">
-                        <div class="col-8 min-vh-100 d-flex flex-column align-items-start justify-content-center">
-                            <h1>Hallo</h1>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-6 min-vh-100 d-flex flex-column align-items-center pt-5">
+                            <img src="{{ asset('assets/img/smartwait-banner.png') }}" style="width: 300px" class="mb-3"
+                                alt="Logo Smart Wait">
+                            <div class="col-10 bg-white d-flex flex-column justify-content-center align-items-start p-4"
+                                style="border-radius: 8px">
+                                <div class="d-flex flex-column align-items-start">
+                                    <h2 class="fw-bold">Welcome</h2>
+                                    <p>Login now to continue</p>
+                                </div>
+                                <img src="{{ asset('assets/img/bg-img.png') }}" class="mx-auto" alt="Banner Image">
+                            </div>
                         </div>
 
-                        <div class="col-4 min-vh-100 d-flex flex-column align-items-center justify-content-center">
+                        <div class="col-6 min-vh-100 d-flex flex-column align-items-center justify-content-center">
                             <div class="col-12">
                                 @if (session()->has('success'))
                                     <div class="alert alert-success" role="alert">
@@ -25,59 +34,55 @@
                                 @endif
                             </div>
 
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="d-flex justify-content-center pt-4 pb-2">
-                                        <div class="d-flex flex-column justify-content-center align-items-center w-auto">
-                                            <img src="{{ asset('assets/img/logo.png') }}" width="150" alt="Logo BPMSPH">
+                            <div class="col-9">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="pb-2">
+                                            <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
                                         </div>
-                                    </div><!-- End Logo -->
 
-                                    <div class="pb-2">
-                                        <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                                    </div>
-
-                                    <form class="row g-3" action="/login" method="post">
-                                        @csrf
-                                        <div class="col-12">
-                                            <label for="email" class="form-label">Email Address</label>
-                                            <div class="input-group has-validation">
-                                                <span class="input-group-text" id="inputGroupPrepend"><i
-                                                        class="bi bi-envelope"></i></span>
-                                                <input type="email" name="email"
-                                                    class="form-control @error('email') is-invalid @enderror" id="email"
-                                                    required autocomplete="off" autofocus required
-                                                    value="{{ old('email') }}" placeholder="Enter your email address!">
-                                                @error('email')
-                                                    <div class="invalid-feedback">{{ $message }}</div>
-                                                @enderror
+                                        <form class="row g-3" action="/login" method="post">
+                                            @csrf
+                                            <div class="col-12">
+                                                <label for="email" class="form-label">Email Address</label>
+                                                <div class="input-group has-validation">
+                                                    <span class="input-group-text" id="inputGroupPrepend"><i
+                                                            class="bi bi-envelope"></i></span>
+                                                    <input type="email" name="email"
+                                                        class="form-control @error('email') is-invalid @enderror"
+                                                        id="email" required autocomplete="off" autofocus required
+                                                        value="{{ old('email') }}" placeholder="Enter your email address!">
+                                                    @error('email')
+                                                        <div class="invalid-feedback">{{ $message }}</div>
+                                                    @enderror
+                                                </div>
                                             </div>
-                                        </div>
 
-                                        <div class="col-12">
-                                            <label for="password" class="form-label">Password</label>
-                                            <div class="input-group has-validation">
-                                                <span class="input-group-text" id="inputGroupPrepend"><i
-                                                        class="bi bi-lock"></i></span>
-                                                <input type="password" name="password" class="form-control" id="password"
-                                                    required placeholder="Enter your password!">
-                                                <div class="invalid-feedback">Please enter your password.</div>
+                                            <div class="col-12">
+                                                <label for="password" class="form-label">Password</label>
+                                                <div class="input-group has-validation">
+                                                    <span class="input-group-text" id="inputGroupPrepend"><i
+                                                            class="bi bi-lock"></i></span>
+                                                    <input type="password" name="password" class="form-control"
+                                                        id="password" required placeholder="Enter your password!">
+                                                    <div class="invalid-feedback">Please enter your password.</div>
+                                                </div>
                                             </div>
+
+                                            <div class="col-12">
+                                                <button class="btn btn-primary w-100" type="submit">Login</button>
+                                            </div>
+                                            <div class="col-12 text-center">
+                                                <p class="small mb-0">Don't have account? <a href="/register">Create
+                                                        an account</a></p>
+                                            </div>
+                                        </form>
+
+                                        <div class="credits text-center pt-4 pb-0">
+                                            Designed by <a href="https://www.instagram.com/chikaadesvii/">Chika Desvi</a>
                                         </div>
 
-                                        <div class="col-12">
-                                            <button class="btn btn-primary w-100" type="submit">Login</button>
-                                        </div>
-                                        <div class="col-12 text-center">
-                                            <p class="small mb-0">Don't have account? <a href="/register">Create
-                                                    an account</a></p>
-                                        </div>
-                                    </form>
-
-                                    <div class="credits text-center pt-4 pb-0">
-                                        Designed by <a href="https://www.instagram.com/chikaadesvii/">Chika Desvi</a>
                                     </div>
-
                                 </div>
                             </div>
 
--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -5,74 +5,80 @@
         <div class="container-fluid">
             <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center">
                 <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-8 min-vh-100 d-flex flex-column align-items-start justify-content-center">
-                            <h1>Hallo</h1>
+                    <div class="row">
+                        <div class="col-6 min-vh-100 d-flex flex-column align-items-center pt-5">
+                            <img src="{{ asset('assets/img/smartwait-banner.png') }}" style="width: 300px" class="mb-3"
+                                alt="Logo Smart Wait">
+                            <div class="col-10 bg-white d-flex flex-column justify-content-center align-items-start p-4"
+                                style="border-radius: 8px">
+                                <div class="d-flex flex-column align-items-start">
+                                    <h2 class="fw-bold">Welcome</h2>
+                                    <p>Login now to continue</p>
+                                </div>
+                                <img src="{{ asset('assets/img/bg-img.png') }}" class="mx-auto" alt="Banner Image">
+                            </div>
                         </div>
 
-                        <div class="col-4 min-vh-100 d-flex flex-column align-items-center justify-content-center">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="d-flex justify-content-center pt-4 pb-2">
-                                        <div class="logo d-flex align-items-center w-auto">
-                                            <img src="{{ asset('assets/img/logo.png') }}" alt="Logo BPMSPH">
+                        <div class="col-6 min-vh-100 d-flex flex-column align-items-center justify-content-center">
+                            <div class="col-9">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="pb-2">
+                                            <h5 class="card-title text-center pb-0 fs-4">Create an Account</h5>
                                         </div>
-                                    </div><!-- End Logo -->
 
-                                    <div class="pb-2">
-                                        <h5 class="card-title text-center pb-0 fs-4">Create an Account</h5>
+                                        <form class="row g-3" action="/register" method="post">
+                                            @csrf
+                                            <div class="col-12">
+                                                <label for="name" class="form-label">Full Name</label>
+                                                <input type="text" name="name"
+                                                    class="form-control @error('name') is-invalid @enderror" id="name"
+                                                    autofocus autocomplete="off" placeholder="Enter your full name!"
+                                                    required value="{{ old('name') }}">
+                                                @error('name')
+                                                    <div class="invalid-feedback">{{ $message }}</div>
+                                                @enderror
+                                            </div>
+
+                                            <div class="col-12">
+                                                <label for="email" class="form-label">Email Address</label>
+                                                <input type="email" name="email"
+                                                    class="form-control @error('email') is-invalid @enderror" id="email"
+                                                    autocomplete="off" placeholder="Enter your email address!" required
+                                                    value="{{ old('email') }}">
+                                                @error('email')
+                                                    <div class="invalid-feedback">{{ $message }}</div>
+                                                @enderror
+                                            </div>
+
+                                            <div class="col-12">
+                                                <label for="password" class="form-label">Password</label>
+                                                <input type="password" name="password"
+                                                    class="form-control @error('password') is-invalid @enderror"
+                                                    id="password" autocomplete="off" placeholder="Enter your password!"
+                                                    required>
+                                                @error('password')
+                                                    <div class="invalid-feedback">{{ $message }}</div>
+                                                @enderror
+                                            </div>
+
+                                            <div class="col-12">
+                                                <button class="btn btn-primary w-100" type="submit">Create Account</button>
+                                            </div>
+
+                                            <div class="col-12 text-center">
+                                                <p class="small mb-0">Already have an account? <a href="/login">Login</a>
+                                                </p>
+                                            </div>
+                                        </form>
+
+                                        <div class="credits text-center pt-4 pb-0">
+                                            Designed by <a href="https://www.instagram.com/chikaadesvii/">Chika Desvi</a>
+                                        </div>
+
                                     </div>
-
-                                    <form class="row g-3" action="/register" method="post">
-                                        @csrf
-                                        <div class="col-12">
-                                            <label for="name" class="form-label">Full Name</label>
-                                            <input type="text" name="name"
-                                                class="form-control @error('name') is-invalid @enderror" id="name"
-                                                autofocus autocomplete="off" placeholder="Enter your full name!" required
-                                                value="{{ old('name') }}">
-                                            @error('name')
-                                                <div class="invalid-feedback">{{ $message }}</div>
-                                            @enderror
-                                        </div>
-
-                                        <div class="col-12">
-                                            <label for="email" class="form-label">Email Address</label>
-                                            <input type="email" name="email"
-                                                class="form-control @error('email') is-invalid @enderror" id="email"
-                                                autocomplete="off" placeholder="Enter your email address!" required
-                                                value="{{ old('email') }}">
-                                            @error('email')
-                                                <div class="invalid-feedback">{{ $message }}</div>
-                                            @enderror
-                                        </div>
-
-                                        <div class="col-12">
-                                            <label for="password" class="form-label">Password</label>
-                                            <input type="password" name="password"
-                                                class="form-control @error('password') is-invalid @enderror" id="password"
-                                                autocomplete="off" placeholder="Enter your password!" required>
-                                            @error('password')
-                                                <div class="invalid-feedback">{{ $message }}</div>
-                                            @enderror
-                                        </div>
-
-                                        <div class="col-12">
-                                            <button class="btn btn-primary w-100" type="submit">Create Account</button>
-                                        </div>
-
-                                        <div class="col-12 text-center">
-                                            <p class="small mb-0">Already have an account? <a href="/login">Login</a></p>
-                                        </div>
-                                    </form>
-
-                                    <div class="credits text-center pt-4 pb-0">
-                                        Designed by <a href="https://www.instagram.com/chikaadesvii/">Chika Desvi</a>
-                                    </div>
-
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 </div>

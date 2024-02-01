b82443926d9052ce6622853959c45d59be0e6ab7
ahdianmirza <ahdianmirza@gmail.com>
2024-02-01 18:44:46 +0700

    Handling 419 error di menu lainnya

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,5 +13,9 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/',
+        '/sample-uji',
+        '/konsultasi-kunjungan',
+        '/ulasan',
+        '/dashboard/antrian'
     ];
 }
